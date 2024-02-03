c40f98a79992d25eb26c923e81d650c0094589d1
hritam.chowdhury <hritam.chowdhury@paytm.com>
2024-02-03 20:33:40 +0530

    day 2

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 
+import React from "react";
+import ReactDom from "react-dom/client";
 // <div id="parent">
 // <div id="child">
 //     <h1>
@@ -15,7 +17,7 @@
 const parent = React.createElement("div", { "id": "parent" }, [
     React.createElement("div",
         { "id": "child" },
-        [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]),
+        [React.createElement("h1", {}, "React rock"),React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]),
     React.createElement("div",
         { "id": "child2" },
         [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")])
@@ -34,5 +36,5 @@ let heading = React.createElement("h1", {
 }, "Hello World from React")
 
 console.log(parent); // it will return a javascript object
-let root = ReactDOM.createRoot(document.getElementById("root"));
+let root = ReactDom.createRoot(document.getElementById("root"));
 root.render(parent);
