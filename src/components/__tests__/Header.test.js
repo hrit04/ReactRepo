76e50977a4d2e67ca90bf652252c7f5072030c7f
hrit04 <hritam.c04@gmail.com>
2024-03-13 22:51:39 +0530

    Assignment 2

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -3,17 +3,44 @@ import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
 
 
 
 
-describe("Header Component test suit", ()=>{
+describe("Header Component test suit", () => {
 
-    it("Should load header component with a login button", ()=>{
-        // render(<Provider store={appStore}> 
-        //        <Header/>
-        //        </Provider>);
-        // const login = screen.getByText("Login");
-        // expect(login).toBeInTheDocument();
+    it("Should load header component with a login button", () => {
+        render(
+            <BrowserRouter>
+                <Provider store={appStore}>
+                    <Header />
+                </Provider>
+            </BrowserRouter>);
+        const login = screen.getByText("Login");
+        expect(login).toBeInTheDocument();
+    })
+
+    it("Should cart items is zero", () => {
+        render(
+            <BrowserRouter>
+                <Provider store={appStore}>
+                    <Header />
+                </Provider>
+            </BrowserRouter>);
+        const cartItems = screen.getByText("Cart 0 items");
+        expect(cartItems).toBeInTheDocument();
+    })
+
+
+    it("Should click login", () => {
+        render(
+              <BrowserRouter>
+                <Provider store={appStore}>
+                    <Header />
+                </Provider>
+            </BrowserRouter>);
+        const cartItems = screen.getByRole("button",);
+        expect(cartItems).toBeInTheDocument();
     })
 })
